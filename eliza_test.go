1d09f7b7a31d3e49241113990b11207b52ad2c37
Casey Rosengren <nocashvaluedrumz@gmail.com>
2014-10-27 16:50:06 -0600

    Added tests for script loading.

--- eliza_test.go
+++ eliza_test.go
@@ -1,7 +1,6 @@
 package eliza
 
 import (
-	"fmt"
 	"testing"
 )
 
@@ -20,5 +19,23 @@ func TestCheckForQuit(t *testing.T) {
 }
 
 func TestScriptLoading(t *testing.T) {
-	fmt.Println(Keywords)
+	// Check that keyword map loading
+	if _, ok := Keywords["xnone"]; ok != true {
+		t.Errorf("Keywords not getting loaded. 'xnone' keyword missing.")
+	}
+
+	// Check that preprocessor map loading
+	if _, ok := Pre["dont"]; ok != true {
+		t.Errorf("Pre not getting loaded. 'dont' keyword missing.")
+	}
+
+	// Check that postprocessor map loading
+	if _, ok := Post["am"]; ok != true {
+		t.Errorf("Post not getting loaded. 'am' keyword missing.")
+	}
+
+	// Check that synonym map loading
+	if _, ok := Synonyms["be"]; ok != true {
+		t.Errorf("Synonyms not getting loaded. 'be' keyword missing.")
+	}
 }
